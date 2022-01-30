5b47ef690fa3f9c8ecd07c4cdefb118c11cb8509
NaOH <NaOH@live.cn>
2022-01-31 02:16:06 +0800

    处理文章分页 50%

--- controller/article.js
+++ controller/article.js
@@ -1,6 +1,20 @@
+const { Article } = require("../model");
+
 exports.showIndex = async (req, res, next) => {
     try {
-        res.render("index.html");
+        const page = req.query.page ? Number.parseInt(req.query.page) : 1;
+        const pageSize = 10;
+        const articlesCount = await Article.countDocuments();
+        const articles = await Article.find()
+            .skip((page - 1) * pageSize)
+            .limit(pageSize);
+        res.render("index", {
+            articles,
+            page,
+            pageSize,
+            articlesCount,
+            totalPage: Math.ceil(articlesCount / pageSize),
+        });
     } catch (err) {
         next(err);
     }
